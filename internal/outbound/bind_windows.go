90e6b974e1e12139597d37799194ece201e65d0a
Sky <skylabward@gmail.com>
2026-08-26 22:45:00 +0700

    fix: encode Windows interface index for unicast sockets

--- internal/outbound/bind_windows.go
+++ internal/outbound/bind_windows.go
@@ -3,6 +3,7 @@
 package outbound
 
 import (
+	"math/bits"
 	"syscall"
 
 	"golang.org/x/sys/windows"
@@ -10,12 +11,18 @@ import (
 
 const ipUnicastIf = 31 // IP_UNICAST_IF
 
+// windowsInterfaceIndexValue converts an interface index to the network-byte
+// order required by the Windows IP_UNICAST_IF socket option.
+func windowsInterfaceIndexValue(ifIndex int) int {
+	return int(bits.ReverseBytes32(uint32(ifIndex)))
+}
+
 func bindToInterface(ifIndex int, ifName string) func(network, address string, c syscall.RawConn) error {
 	return func(network, address string, c syscall.RawConn) error {
 		var innerErr error
-		idx := uint32(ifIndex)
+		idx := windowsInterfaceIndexValue(ifIndex)
 		err := c.Control(func(fd uintptr) {
-			innerErr = windows.SetsockoptInt(windows.Handle(fd), windows.IPPROTO_IP, ipUnicastIf, int(idx))
+			innerErr = windows.SetsockoptInt(windows.Handle(fd), windows.IPPROTO_IP, ipUnicastIf, idx)
 		})
 		if err != nil {
 			return err
